e61afcd2dca4cab86bb2f4126a34b372c5604c51
shaikhanis <anisshaikh19059@gmail.com>
2023-12-11 10:58:32 +0530

    do nothing

---
+++ java basics/src/loops/forLops.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class forLops {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i<=10; i++){
+            sum= i+sum;
+            System.out.println(sum);
+        }
+    }
+}
