733165c9652f178c8feed0509c43ac4f651f7e28
shaikhanis <anisshaikh19059@gmail.com>
2023-12-24 13:53:04 +0530

    modify some code

--- java basics/src/patterns/NumberPattern.java
+++ java basics/src/patterns/NumberPattern.java
@@ -1,17 +1,26 @@
 package patterns;
-
+/*1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+ */
 public class NumberPattern {
     public static void main(String[] args) {
         int n=6;
-        for (int i=1; i<=n; i++){
+        for (int i=1; i<n; i++){
             for (int j=1; j<=n-i; j++){
                 System.out.print(j+" ");
             }
             System.out.println();
         }
 
-        for (int i=2; i<=n; i++) {
-            for (int j = 1; j <=i; j++) {
+        for (int k=2; k<n; k++) {
+            for (int j = 1; j <=k; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
