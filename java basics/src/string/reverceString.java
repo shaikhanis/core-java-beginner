a9ce2892a5d381f557bba6b75804890889458f06
shaikhanis <anisshaikh19059@gmail.com>
2023-12-28 20:54:15 +0530

    do nothing

--- java basics/src/string/reverceString.java
+++ java basics/src/string/reverceString.java
@@ -4,11 +4,28 @@ public class reverceString {
     public static void main(String[] args) {
         String s = "anis";
 
-        char [] ch = s.toCharArray();
-        String reverce = "";
-        for (int i=s.length()-1; i>=0; i--){
-            reverce = reverce +  s.charAt(i);
+//        String reverce = "";
+//        char [] ch = s.toCharArray();
+//        for (int i=s.length()-1; i>= 0; i--){
+//            reverce = reverce+s.charAt(i);
+//        }
+//        System.out.print(reverce);
+
+        StringBuilder sbr = new StringBuilder(s);
+        sbr.reverse();
+        System.out.print(sbr);
+
+        StringBuilder sbd = new StringBuilder();
+        sbd.append(s);
+
+        sbd.reverse();
+        System.out.print(sbd);
+
+        if (sbd.toString().equals(s)){
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
         }
-        System.out.print(reverce);
     }
 }
+
